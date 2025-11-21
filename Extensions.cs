3a4e2c62731c77eddf578afab68ad74173e55c4f
Thorsten <thorsten@textcontrol.com>
2025-11-21 10:37:17 +0100

    Moving Extensions class into separate file

---
+++ Extensions.cs
@@ -0,0 +1,32 @@
+﻿using HotPixels.Imaging.Dithering;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotPixels.Imaging;
+
+/// <summary>
+/// Useful extension methods
+/// </summary>
+internal static class Extensions {
+
+   /// <summary>
+   /// Returns whether the dither mode is an ordered dither mode.
+   /// </summary>
+   public static bool IsOrderedDither(this DitherMode mode) => mode >= DitherMode.Bayer2x2;
+
+   /// <summary>
+   /// Calculates the perceived brightness of a color.
+   /// </summary>
+   /// <param name="color">The color.</param>
+   /// <param name="gamma">The gamma correction factor. Default is 0.75. Higher values make the image darker.</param>
+   /// <returns>The perceived brightness as a byte value (0-255).</returns>
+   public static byte GetPerceivedBrightness(this Color color, double gamma = Program.DefaultGamma) {
+      double brightness = color.R * 0.299 + color.G * 0.587 + color.B * 0.114;
+      brightness = Math.Pow(brightness / 255.0, gamma) * 255.0;
+      return (byte) brightness;
+   }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Text;
-using System.Drawing;
+﻿using HotPixels.Imaging;
 using HotPixels.Imaging.Dithering;
 using HotPixels.Printing;
-using System.Globalization;
+using System.Drawing;
 using System.Drawing.Printing;
+using System.Globalization;
+using System.Text;
 
 class Program {
 
@@ -210,25 +210,3 @@ class Program {
    }
 }
 
-/// <summary>
-/// Useful extension methods
-/// </summary>
-internal static class Extensions {
-
-   /// <summary>
-   /// Returns whether the dither mode is an ordered dither mode.
-   /// </summary>
-   public static bool IsOrderedDither(this DitherMode mode) => mode >= DitherMode.Bayer2x2;
-
-   /// <summary>
-   /// Calculates the perceived brightness of a color.
-   /// </summary>
-   /// <param name="color">The color.</param>
-   /// <param name="gamma">The gamma correction factor. Default is 0.75. Higher values make the image darker.</param>
-   /// <returns>The perceived brightness as a byte value (0-255).</returns>
-   public static byte GetPerceivedBrightness(this Color color, double gamma = Program.DefaultGamma) {
-      double brightness = color.R * 0.299 + color.G * 0.587 + color.B * 0.114;
-      brightness = Math.Pow(brightness / 255.0, gamma) * 255.0;
-      return (byte) brightness;
-   }
-}
